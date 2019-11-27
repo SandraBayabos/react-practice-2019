02d51e8e8555eb1044dca9f0fba01a638088b428
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-27 15:01:49 +0800

    change navbar icon & title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 // convert function App() to class App for now & we need to extend the React.Component (but if we import {Component from above then can just say Component})
 class App extends Component {
@@ -14,8 +15,8 @@ class App extends Component {
       // JSX must only have ONE parent element, otherwise get warning. So everything must go inside this one div. In chrome developer you'll see everything, including the App div is wrapped around a div called "root".
       //So can replease <div className="App"><div> with <React.Fragment></React.Fragment>, <Fragment></Fragment> or just <> so that it goes directly from "root" and everything is rendered within
       <div className="App">
-        <h1></h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        {/* title is a prop that we will pass down to Navbar.js */}
+        <Navbar title="Github Finder" />
       </div>
     );
   }
