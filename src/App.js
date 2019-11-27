b5a27844aedbb63d2fcb427f8b47b47f7ba75e62
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-27 14:42:06 +0800

    edited App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+// convert function App() to class App for now & we need to extend the React.Component (but if we import {Component from above then can just say Component})
+class App extends Component {
+  // render() is a lifecycle method that runs when all the other components are loaded
+  render() {
+    //All javscript goes under the render() including variables, functions etc.
+    const name = "Jupiter Moon";
+    const loading = false;
+    const showName = false;
+
+    return (
+      // JSX must only have ONE parent element, otherwise get warning. So everything must go inside this one div. In chrome developer you'll see everything, including the App div is wrapped around a div called "root".
+      //So can replease <div className="App"><div> with <React.Fragment></React.Fragment>, <Fragment></Fragment> or just <> so that it goes directly from "root" and everything is rendered within
+      <div className="App">
+        <h1></h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
