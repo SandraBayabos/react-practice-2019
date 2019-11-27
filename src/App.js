3a3453275d9f52befebe9a35bd1ad58d39c04b70
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-27 16:32:16 +0800

    used PropTypes to pass props

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ class App extends Component {
   // render() is a lifecycle method that runs when all the other components are loaded
   render() {
     //All javscript goes under the render() including variables, functions etc.
-    const name = "Jupiter Moon";
-    const loading = false;
-    const showName = false;
 
     return (
       // JSX must only have ONE parent element, otherwise get warning. So everything must go inside this one div. In chrome developer you'll see everything, including the App div is wrapped around a div called "root".
       //So can replease <div className="App"><div> with <React.Fragment></React.Fragment>, <Fragment></Fragment> or just <> so that it goes directly from "root" and everything is rendered within
       <div className="App">
         {/* title is a prop that we will pass down to Navbar.js */}
-        <Navbar title="Github Finder" />
+        <Navbar />
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Navbar extends Component {
+  //if don't want to pass down props you can use static defaultProps or static propTypes. Both will work
+
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className="navbar bg-primary">
         <h1>
-          <i className="fab fa-github" /> {this.props.title}
+          <i className={this.props.icon} /> {this.props.title}
         </h1>
       </nav>
     );
