d2723bee3c36dd472c1c880c30706fe08c874067
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-27 18:26:05 +0800

    destructured code

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,28 +2,31 @@ import React, { Component } from "react";
 
 class UserItem extends Component {
   // constructor runs only when the component runs
-  constructor() {
-    super();
-    console.log("Hi");
-    this.state = {
-      id: "id",
-      login: "mojombo",
-      avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-      html_url: "https://github.com/mojombo"
-    };
-  }
+  // can take out constructor & super and this.state and just return state = blach also and get the same result
+  // constructor() {
+  //   super();
+  // console.log("Hi");
+  state = {
+    id: "id",
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo"
+  };
+  // }
   render() {
+    // can also destructure this.state.whatever by doing the following:-
+    const { login, avatar_url, html_url } = this.state;
     return (
       <div className="card text-center">
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt=""
           className="round-img"
           style={{ width: "60px" }}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
         <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
             More
           </a>
         </div>
