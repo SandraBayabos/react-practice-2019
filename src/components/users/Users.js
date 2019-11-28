22f50fb92c492e6a9eb5fc202d031701ae787f4c
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-28 15:49:19 +0800

    added loading spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={usersStyle}>
         {/* Loop through the list in the state using .map */}
         {/* each child in a list needs to have a unique key, so in this case we use user.id */}
         {/* user={user} is we are passing in the entire user - each {user} represents the entire user object after you've looped */}
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 // creating a variable for usersStyle
 
