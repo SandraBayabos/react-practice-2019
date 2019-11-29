55c91d22936f0e1c62538689b4b93219e501f76f
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-11-29 18:03:11 +0800

    edited Search.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
-export class Search extends Component {
+// FORMS ARE ALWAYS COMPONENT-LEVEL STATE. YOU WON'T BRING IT TO APP.JS LEVEL
+
+class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  // if you don't use an arrow function, you need to bind "this" to the state or else "this" will be undefined
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.text);
+  }
+
   render() {
     return (
       <div>
-        <form className="form">
-          <input type="text" name="text" placeholder="Search Users" />
+        <form onSubmit={this.onSubmit.bind(this)} className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type="submit"
             value="Search"
