ea3af87849088fa7dddb02d24c6f7688534f52c2
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-12-03 14:49:37 +0800

    added hireable checkbox

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -41,6 +41,12 @@ class User extends Component {
         <Link to="/" className="btn btn-light">
           Back To Search
         </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-check text-danger" />
+        )}
       </Fragment>
     );
   }
